2d0f6b998010ee361a698377923278cd6be408a1
Deepukumar93 <deepu95760@gmail.com>
2025-07-11 21:28:06 +0530

    js update

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+// function shopNow() {
+//   alert("Redirecting to shop page...");
+//   // You can redirect to another page like this:
+//   // window.location.href = "shop.html";
+// }
+
+function toggleMenu() {
+  const navLinks = document.getElementById('navLinks');
+  navLinks.classList.toggle('show');
+}
+
 function shopNow() {
   alert("Redirecting to shop page...");
-  // You can redirect to another page like this:
-  // window.location.href = "shop.html";
+  // window.location.href = 'shop.html'; // Uncomment if needed
 }
